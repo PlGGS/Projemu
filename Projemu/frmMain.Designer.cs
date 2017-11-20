8f3cc6bd2e0c8b01f6140726a8473fd7f8083a25
PlGGS <bbor0422@gmail.com>
2017-11-19 20:16:06 -0600

    Added files via upload

---
+++ Projemu/frmMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Projemu
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkShowMenuStrip = new System.Windows.Forms.CheckBox();
+            this.lblShowMenuStrip = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxScreenNum = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // chkShowMenuStrip
+            // 
+            this.chkShowMenuStrip.AutoSize = true;
+            this.chkShowMenuStrip.Checked = true;
+            this.chkShowMenuStrip.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowMenuStrip.Location = new System.Drawing.Point(151, 9);
+            this.chkShowMenuStrip.Name = "chkShowMenuStrip";
+            this.chkShowMenuStrip.Size = new System.Drawing.Size(15, 14);
+            this.chkShowMenuStrip.TabIndex = 7;
+            this.chkShowMenuStrip.UseVisualStyleBackColor = true;
+            this.chkShowMenuStrip.CheckedChanged += new System.EventHandler(this.chkShowMenuStrip_CheckedChanged);
+            // 
+            // lblShowMenuStrip
+            // 
+            this.lblShowMenuStrip.AutoSize = true;
+            this.lblShowMenuStrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowMenuStrip.Location = new System.Drawing.Point(10, 9);
+            this.lblShowMenuStrip.Name = "lblShowMenuStrip";
+            this.lblShowMenuStrip.Size = new System.Drawing.Size(110, 16);
+            this.lblShowMenuStrip.TabIndex = 6;
+            this.lblShowMenuStrip.Text = "Show Menu Strip:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Gameplay Screen:";
+            // 
+            // cbxScreenNum
+            // 
+            this.cbxScreenNum.FormattingEnabled = true;
+            this.cbxScreenNum.Location = new System.Drawing.Point(135, 32);
+            this.cbxScreenNum.Name = "cbxScreenNum";
+            this.cbxScreenNum.Size = new System.Drawing.Size(30, 21);
+            this.cbxScreenNum.TabIndex = 9;
+            this.cbxScreenNum.SelectionChangeCommitted += new System.EventHandler(this.cbxScreenNum_SelectionChangeCommitted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 63);
+            this.Controls.Add(this.cbxScreenNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkShowMenuStrip);
+            this.Controls.Add(this.lblShowMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projemu - By: Blake Boris";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkShowMenuStrip;
+        private System.Windows.Forms.Label lblShowMenuStrip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxScreenNum;
+    }
+}
+
---
+++ Projemu/frmMain.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using IWshRuntimeLibrary;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Projemu
+{
+    public partial class frmMain : Form
+    {
+        //Import window changing function
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        //Import find window finding function
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        //Import force window draw function
+        [DllImport("user32.dll")]
+        static extern bool DrawMenuBar(IntPtr hWnd);
+
+        //Import window placement function
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        private const int gwlStyle = -16;
+        private const int wsBorder = 0x00800000;
+        private const int wsCaption = 0x00C00000;
+        private const int wsSysMenu = 0x00080000;
+        private const int wsMinimizeBox = 0x00020000;
+        private const int wsMenuStrip = 19;
+        private int projectorScreen = Properties.Settings.Default.screenNum;
+        Screen[] screens = Screen.AllScreens;
+        IntPtr[] cemuWindows;
+
+        /// <summary>
+        /// Creates a new shortcut of a selected program. In this case we use it to create Halo Online shortcuts that don't need to be run by the default launcher.
+        /// </summary>
+        /// <param name="shortcutName"></param>
+        /// <param name="shortcutPath"></param>
+        /// <param name="shortcutTargetFileLocation"></param>
+        /// <param name="shortcutArguments"></param>
+        /// <param name="shortcutStartInDirectory"></param>
+        /// <param name="shortcutIconLocation"></param>
+        /// <param name="shortcutDescription"></param>
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string shortcutTargetFileLocation, string shortcutArguments, string shortcutStartInDirectory, string shortcutIconLocation, string shortcutDescription)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = shortcutDescription;
+            shortcut.IconLocation = shortcutIconLocation;
+            shortcut.TargetPath = shortcutTargetFileLocation;
+            shortcut.Arguments = shortcutArguments;
+            shortcut.WorkingDirectory = shortcutStartInDirectory;
+            shortcut.Save();
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Taskbar.Show();
+            this.Icon = Properties.Resources.Projemu;
+            cemuWindows = new IntPtr[Process.GetProcessesByName("cemu").Length + 1];
+
+            for (int i = 0; i < screens.Length; i++)
+            {
+                cbxScreenNum.Items.Add(i + 1);
+            }
+
+            cbxScreenNum.SelectedIndex = Properties.Settings.Default.screenNum;
+
+            if (Properties.Settings.Default.appRunBefore == false)
+            {
+                CreateShortcut("Projemu", Environment.GetFolderPath(Environment.SpecialFolder.Desktop), Application.StartupPath + "\\Projemu.exe", "", Application.StartupPath, Application.StartupPath + "\\Projemu.ico", "Cemu on your projector! Woah!");
+                Properties.Settings.Default.appRunBefore = true;
+                Properties.Settings.Default.Save();
+            }
+
+            Process.Start(Application.StartupPath + "\\Cemu");
+            Process.GetProcessesByName("cemu")[0].WaitForInputIdle();
+            for (int i = 0; i < Process.GetProcessesByName("cemu").Length; i++)
+            {
+                cemuWindows[i] = Process.GetProcessesByName("cemu")[i].MainWindowHandle;
+            }
+
+            for (int i = 0; i < cemuWindows.Length; i++)
+            {
+                SetWindowLong(cemuWindows[i], gwlStyle, wsSysMenu);
+                SetWindowPos(cemuWindows[i], 0, screens[projectorScreen].Bounds.X, screens[projectorScreen].Bounds.Y, screens[projectorScreen].Bounds.Width, screens[projectorScreen].Bounds.Height, 0x0040);
+                DrawMenuBar(cemuWindows[i]);
+            }
+        }
+        
+        private void setWindow()
+        {
+            if (chkShowMenuStrip.CheckState == CheckState.Checked)
+            {
+                try
+                {
+                    for (int i = 0; i < cemuWindows.Length; i++)
+                    {
+                        SetWindowLong(cemuWindows[i], gwlStyle, wsSysMenu);
+                        SetWindowPos(cemuWindows[i], 0, screens[projectorScreen].Bounds.X, screens[projectorScreen].Bounds.Y, screens[projectorScreen].Bounds.Width, screens[projectorScreen].Bounds.Height + wsMenuStrip, 0x0040);
+                        DrawMenuBar(cemuWindows[i]);
+                        Taskbar.Hide();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to properly place Cemu into borderless window mode.", "Cemu Borderless Window");
+                }
+            }
+            else
+            {
+                try
+                {
+                    for (int i = 0; i < cemuWindows.Length; i++)
+                    {
+                        SetWindowLong(cemuWindows[i], gwlStyle, wsSysMenu);
+                        SetWindowPos(cemuWindows[i], 0, screens[projectorScreen].Bounds.X, screens[projectorScreen].Bounds.Y - wsMenuStrip, screens[projectorScreen].Bounds.Width, screens[projectorScreen].Bounds.Height + wsMenuStrip, 0x0040);
+                        DrawMenuBar(cemuWindows[i]);
+                        Taskbar.Hide();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to properly place Cemu into borderless window mode.", "Cemu Borderless Window");
+                }
+            }
+        }
+
+        private void chkShowMenuStrip_CheckedChanged(object sender, EventArgs e)
+        {
+            setWindow();
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Taskbar.Show();
+        }
+
+        private void cbxScreenNum_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            projectorScreen = cbxScreenNum.SelectedIndex;
+            Properties.Settings.Default.screenNum = cbxScreenNum.SelectedIndex;
+            Properties.Settings.Default.Save();
+            setWindow();
+        }
+    }
+}
